ef7dc75e75bac219487e8e5cf28e8bdecde4e513
Buğrahan <bugraata2344@gmail.com>
2023-11-09 02:16:16 +0300

    General Update

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favorite_places/models/place.dart';
 
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+
+
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
+  void addPlace(String title, File image) async{
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: copiedImage);
     state = [newPlace, ...state];
   }
 }
